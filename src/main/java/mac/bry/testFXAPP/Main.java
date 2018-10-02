017fc62039f0613023566c1c36fd04f3cf9b29ce
Maciej Bryja <maciej.bryja@wp.pl>
2018-10-02 17:31:22 +0200

    create StackPane  with fxml file

--- src/main/java/mac/bry/testFXAPP/Main.java
+++ src/main/java/mac/bry/testFXAPP/Main.java
@@ -1,6 +1,11 @@
 package mac.bry.testFXAPP;
 
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +17,28 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		//Stack Pane in pure Java
+		//****************************************//
+		
+		//StackPane stackPane = new StackPane();
+		//Button button = new Button("Przycisk");
+		//stackPane.getChildren().add(button);
+		//Scene scene = new Scene(stackPane,400,400);
+		//primaryStage.setScene(scene);
+		
+		
+		//Stack Pane from FXML file
+		//****************************************//
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		fxmlLoader.setLocation(this.getClass().getResource("/fxml/stackPaneWindow.fxml"));
+		StackPane stackPane = fxmlLoader.load();
+		Scene scene = new Scene(stackPane,400,400);
+		primaryStage.setScene(scene);
+		
+		primaryStage.setTitle("First JavaFx app");
 		primaryStage.show();
+		
 		
 	}
 
