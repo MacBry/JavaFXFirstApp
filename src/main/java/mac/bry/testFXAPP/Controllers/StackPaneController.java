129a967943f044f3616dfc095d27f111e7b59774
Maciej Bryja <maciej.bryja@wp.pl>
2018-10-05 20:16:45 +0200

    add some practice method

--- src/main/java/mac/bry/testFXAPP/Controllers/StackPaneController.java
+++ src/main/java/mac/bry/testFXAPP/Controllers/StackPaneController.java
@@ -2,11 +2,19 @@ package mac.bry.testFXAPP.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class StackPaneController {
 
 	@FXML
 	private Button button;
+	
+	@FXML
+	private Label labael1;
+	
+	@FXML
+	private VBox VBOX;
 	
 	public StackPaneController() {
 		System.out.println("I am a Controller");
@@ -20,10 +28,17 @@ public class StackPaneController {
 	@FXML
 	public void onActionButton() {
 		System.out.println("On Action method");
+		labael1.setText("onActionButton");
 	}
 	
 	@FXML
 	public void onMouseEnteredButton() {
 		System.out.println("you are pointing button - onMouseEnteredButton method");
+		labael1.setText("you are pointing button - onMouseEnteredButton method");
+	}
+	@FXML
+	public void VBoxOnMouseEntered() {
+		System.out.println("you are pointing button - VBoxOnMouseEntered method");
+		labael1.setText("you are pointing nothing - VBoxOnMouseEntered");
 	}
 }
